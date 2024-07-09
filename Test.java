fc483a286e1e6074a6ddca4dab27fc83f3d9a4fd
zhuzihaoIT <ITZhuZihao@163.com>
2024-07-09 13:03:38 +0800

    本地提交Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 public class Test{
   public static void main(String[] args){
       System.out.println("这是一个test测试类");
+      System.out.println("这是在本地进行修改的Test");
+      int a = 12;
+      int b = 13;
+      System.out.println("a+b = "+a+b);
   }
 }
